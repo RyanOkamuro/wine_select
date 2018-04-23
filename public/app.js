f3f75e143d213b7410a155a4304811e692883abf
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-04-22 21:51:44 -0700

    Add wine query search

---
+++ public/app.js
@@ -0,0 +1,25 @@
+
+function wineQuery() {
+    let wineSearch = `
+    <section role='region' class='wineLabelRedWhite'>
+        <form role='form' class='wineBrand'>
+            <fieldset name='wineLabel'>
+            <legend>Wine Label Search</legend>
+            <label for='js-wine-label' class='winery'>Wine Label</label>
+            <input placeholder='Layer Cake' type='text' name='js-wine-label' id='js-wine-label' autofocus/>
+            <button role="button" type="submit" class="js-label-search">Submit</button>
+            </fieldset>
+        </form>
+    </section>
+    `;
+}
+
+function startSearchWindow() {
+    $('.login-form').submit(event => {
+        event.preventDefault();
+        let outputElem = $('#red-white');
+        outputElem
+            .prop('hidden', false)
+            .html(wineQuery);
+    })
+}
