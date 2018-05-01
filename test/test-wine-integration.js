c0f908da3fb4e495fa45846808fc91be96a28715
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-05-01 09:10:02 -0700

    Update generateWineData() to match schema

--- test/test-wine-integration.js
+++ test/test-wine-integration.js
@@ -19,6 +19,7 @@ function seedWineData() {
   for (let i=1; i<=4; i++) {
     seedData.push(generateWineData());
   }
+  console.log(seedData);
   return wineListRouter.insertMany(seedData);
 }
 
@@ -28,7 +29,7 @@ function generateBrand() {
   return brand[Math.floor(Math.random() * brand.length)];
 }
 
-function generatewineName() {
+function generateWineName() {
   const wineName = [
     'Firepeak', 'Etiqueta Negra Gran Reserva', 'Primitivo', 'Bin 2'];
   return wineName[Math.floor(Math.random() * wineName.length)];
@@ -37,77 +38,84 @@ function generatewineName() {
 function generateColor() {
   const color = [
     'Red', 'Red', 'Red', 'Red'];
-  return generateColor[Math.floor(Math.random() * generateColor.length)];
+  return color[Math.floor(Math.random() * color.length)];
 }
 
 function generateType() {
   const type = [
     'Pinot Noir', 'Cabernet Sauvignon', 'Primitivo', 'Bin 2'];
-  return generateType[Math.floor(Math.random() * generateType.length)];
+  return type[Math.floor(Math.random() * type.length)];
 }
 
 function generateRating() {
   const rating = [
     5, 4.6, 3.9, 3.7];
-  return generateRating[Math.floor(Math.random() * generateRating.length)];
+  return rating[Math.floor(Math.random() * rating.length)];
 }
 
 function generateAveragePrice() {
   const averagePrice = [
     19, 29.99, 15, 19.99];
-  return generateAveragePrice[Math.floor(Math.random() * generateAveragePrice.length)];
+  return averagePrice[Math.floor(Math.random() * averagePrice.length)];
 }
 
 function generateRegion() {
   const region = [
     'Edna Valley', 'Maipo Valley', 'Puglia', 'South Australia'];
-  return generateRegion[Math.floor(Math.random() * generateRegion.length)];
+  return region[Math.floor(Math.random() * region.length)];
 }
 
 function generateCountry() {
   const country = [
     'USA', 'Chile', 'Italy', 'Australia'];
-    return generateCountry[Math.floor(Math.random() * generateCountry.length)];
+    return country[Math.floor(Math.random() * country.length)];
 }
 
 function generateYear() {
   const year = [
     '2014', '2014', '2014', '2012'];
-  return generateYear[Math.floor(Math.random() * generateYear.length)];
+  return year[Math.floor(Math.random() * year.length)];
 }
 
 function generateFoodSuggestion() {
   const foodSuggestion = [
     'Chicken and Turkey', 'Lamb', 'Beef and Chicken', 'Beef and Lamb'];
-  return generateFoodSuggestion[Math.floor(Math.random() * generateFoodSuggestion.length)]; 
+  return foodSuggestion[Math.floor(Math.random() * foodSuggestion.length)]; 
 }
 
 function generateImage() {
   const image = [
     'https://www.winetransit.com/media/catalog/product/cache/926507dc7f93631a094422215b778fe0/b/a/baileyana_firepeak_pnoir_mv_750.png', 'https://files.thewinebowgroup.com/PDF/brand5002/T0008448_tarapaca-g-reserva-etiq-negra-s-f-jpg-1.jpg', 'https://www.b-21.com/labels/live/ITLCPR14AE.jpg', 'https://www.nataliemaclean.com/images/winepicks/058ba29dde3c11dc0f1c8adf3ae14ed6/original_penfolds-bin-2-shiraz-mourv-dre-2012-209478-bottle-1415126015.jpg'];
-  return generateImage[Math.floor(Math.random() * generateImage.length)];
+  return image[Math.floor(Math.random() * image.length)];
   }
 
 function generateHistory() {
   const history = [
     'The flagship winery from the Niven Family, Baileyana was created over 30 years ago by our matriarch, Catharine Niven, and is now in the expert hands of the second and third generations of her family. An expression of classic cool climate varietals, each wine is sourced from select blocks from our estate Paragon Vineyard—planted in 1973 by Catharine’s husband, Jack. Baileyana’s range of Chardonnays, Pinot Noirs, Syrahs,and sparkling wines are all crafted by our dynamic winemaking team. Refined, rich, and perfectly balanced, Baileyana wines emulate the spirited woman who brought them to life. Boldly Beautiful.', 'Since we were founded at the foothills of the Andes mountain range in 1874, we have crafted elegant, quality wines, becoming one of the most traditional and historical Chilean wineries, as well as a benchmark for viticulture in Chile and over 50 countries.', 'At Layer Cake, we work directly with the farmers to grow the fruit we work with. Our grapes are grown to exacting standards in some of the most diversely-layered vineyards around the world. They are handpicked, separated and fermented with care, then aged in French Oak. The character of each Layer Cake wine is influenced by the vineyard soil, which is layered like a cake…every layer tells a story.', 'The success of Penfolds has been driven by the generations of visionaries and innovators. From the beginning in 1844 to today, the merging of science, art and innovation has driven Penfolds to become one of Australia’s most famed and respected winemakers.'];
-  return generateHistory[Math.floor(Math.random() * generateHistory.length)];
+  return history[Math.floor(Math.random() * history.length)];
   }
 
 function generateInformation() {
   const moreInformation = [
     'http://baileyana.com/', 'http://www.tarapaca.cl/en/history/', 'http://layercakewines.com/about/', 'https://www.penfolds.com/en-us/about-penfolds/heritage/our-history'];
-  return generateInformation[Math.floor(Math.random() * generateInformation.length)];
+  return moreInformation[Math.floor(Math.random() * moreInformation.length)];
   }
 
   function generateWineData() {
     return {
-      "Wine Label": generateBrand(),
-      "Type": generateType(),
-      "Rating": generateRating(),
-      "Price": generateAveragePrice(),
-      "Region": generateCountry(),
-      "Year": generateYear()
+      'brand': generateBrand(),
+      'wineName': generateWineName(),
+      'color': generateColor(),
+      'type': generateType(),
+      'rating': generateRating(),
+      'averagePrice': generateAveragePrice(),
+      'region': generateRegion(),
+      'country': generateCountry(),
+      'year': generateYear(),
+      'foodSuggestion': generateFoodSuggestion(),
+      'image': generateImage(),
+      'history': generateHistory(),
+      'moreInformation': generateInformation(),
     };
   }
 
