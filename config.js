dda9fb4c0417929897375fe521fb88f14cbaf64b
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-05-16 10:54:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
-//require('dotenv').config();
+require('dotenv').config();
 exports.DATABASE_URL = 
     process.env.DATABASE_URL || 
     global.DATABASE_URL || 
     'mongodb://localhost/wine-select';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-wine-select';
 exports.PORT = process.env.PORT || 8080;
-//exports.JWT_SECRET = process.env.JWT_SECRET;
-//exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
